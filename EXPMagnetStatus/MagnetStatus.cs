8529f280ef599fd93f159d1864f4e1cc43c8c822
Clover <blazelion3@gmail.com>
2018-05-19 19:22:45 -0400

    Created Magnet Status Class.

---
+++ EXPMagnetStatus/MagnetStatus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using RetrieveBitmap;
+using LHCEnums;
+
+namespace EXPMagnetStatus
+{
+    public static class MagnetStatus
+    {
+        private static bool GetMagnetStatusPageAsync(out Bitmap expMagnetImg)
+        {
+            if (!Retrieve.GetBitMap("https://vistar-capture.web.cern.ch/vistar-capture/lhcexpmag.png", out expMagnetImg))
+                return false;
+            return true;
+        }
+    }
+}
