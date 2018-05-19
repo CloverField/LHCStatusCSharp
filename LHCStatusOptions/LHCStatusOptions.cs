df57a53ba33506ca6d0d665b166d66a062d76840
Clover <blazelion3@gmail.com>
2018-05-19 18:57:26 -0400

    Added LHCStatusOptions to the project.

---
+++ LHCStatusOptions/LHCStatusOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace LHCStatusOptions
+{
+    public class StatusOptions
+    {
+        public const string CryoStatus = "Cryo Status";
+        public const string PCPermitStatus = "60 AMP PC Permit Status";
+        public const string RFStatus = "RF Status";
+        public const string BeamDumpStatus = "Beam Dump Status";
+    }
+}
