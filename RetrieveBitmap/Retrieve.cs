4e30a36d06b4bc7b70cc50e21d236724930a19ad
Clover <blazelion3@gmail.com>
2018-05-19 10:53:24 -0400

    Created Retrieve class, this will retrieve all bitmaps/ data going forward.

---
+++ RetrieveBitmap/Retrieve.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetrieveBitmap
+{
+    public static class Retrieve
+    {
+        public static bool GetBitMap(string address, out Bitmap bitmap)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                bitmap = null;
+                var result = wc.DownloadDataTaskAsync(address);
+                if (!result.Wait(3000))
+                    return false;
+
+                if (result.IsFaulted)
+                {
+                    if (result.Exception != null)
+                    {
+                        Console.WriteLine("Failed to get Bitmap with Exception: {0}", result.Exception);
+                        return false;
+                    }
+                    Console.WriteLine("Failed to retrieve  BitMap");
+                    return false;
+                }
+
+                bitmap = (Bitmap)Bitmap.FromStream(new MemoryStream(result.Result));
+                return true;
+            }
+        }
+    }
+}
