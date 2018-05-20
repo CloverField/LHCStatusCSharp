e08feaa046891982f8f836275807c0ca5897b2b7
Clover <blazelion3@gmail.com>
2018-05-19 21:31:34 -0400

    Implemented Page 1 Checks.

--- LHCStatus/Program.cs
+++ LHCStatus/Program.cs
@@ -9,6 +9,7 @@ using LHCEnums;
 using LHCStatusOptions;
 using BeamDump;
 using EXPMagnetStatus;
+using Page1Status;
 
 namespace LHCStatus
 {
@@ -249,6 +250,116 @@ namespace LHCStatus
                                 Console.WriteLine("The LHCb Dipole is functioning correctly");
                             else
                                 Console.WriteLine("Looks like there is a fault in the LHCb Dipole.");
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case "6":
+                    Console.WriteLine("Which beam do you want to check?");
+                    var beamValues = Enum.GetValues(typeof(Machine.Page1.Beam)).Cast< Machine.Page1.Beam> ();
+                    i = 1;
+                    beamValues.ToList().ForEach(s => Console.WriteLine(i++ + "." + s.ToString()));
+                    input = Console.ReadLine();
+                    switch (input)
+                    {
+                        case "1":
+                            if (PageStatus.GetBeamSMPStatus(Machine.Page1.Beam.Beam1))
+                                Console.WriteLine("Beam 1's SMP Status is good.");
+                            else
+                                Console.WriteLine("There is a fault with Beam 1's SMP Status.");
+                            break;
+                        case "2":
+                            if (PageStatus.GetBeamSMPStatus(Machine.Page1.Beam.Beam2))
+                                Console.WriteLine("Beam 2's SMP Status is good.");
+                            else
+                                Console.WriteLine("There is a fault with Beam 2's SMP Status.");
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case "7":
+                    Console.WriteLine("Which beam do you want to check?");
+                    var beamValues2 = Enum.GetValues(typeof(Machine.Page1.Beam)).Cast<Machine.Page1.Beam>();
+                    i = 1;
+                    beamValues2.ToList().ForEach(s => Console.WriteLine(i++ + "." + s.ToString()));
+                    input = Console.ReadLine();
+                    switch (input)
+                    {
+                        case "1":
+                            Console.WriteLine("Which SMP Flag do you want to check?");
+                            var beam1SmpFlags = Enum.GetValues(typeof(Machine.Page1.SMPFlags)).Cast<Machine.Page1.SMPFlags>();
+                            i = 1;
+                            beam1SmpFlags.ToList().ForEach(s => Console.WriteLine(i++ + "." + s.ToString().Replace('_',' ')));
+                            input = Console.ReadLine();
+                            bool beam1status;
+                            switch (input)
+                            {
+                                case "1":
+                                    beam1status = PageStatus.GetSMPStatus(Machine.Page1.Beam.Beam1, Machine.Page1.SMPFlags.Beam_Presence);
+                                    Console.WriteLine("Beam 1 Beam Presence Flag is {0}.", beam1status);
+                                    break;
+                                case "2":
+                                    beam1status = PageStatus.GetSMPStatus(Machine.Page1.Beam.Beam1, Machine.Page1.SMPFlags.Global_Beam_Permit);
+                                    Console.WriteLine("Beam 1 Global Beam Permit Flag is {0}.", beam1status);
+                                    break;
+                                case "3":
+                                    beam1status = PageStatus.GetSMPStatus(Machine.Page1.Beam.Beam1, Machine.Page1.SMPFlags.Link_Status_of_Beam_Permits);
+                                    Console.WriteLine("Beam 1 Link Status of Beam Permits Flag is {0}.", beam1status);
+                                    break;
+                                case "4":
+                                    beam1status = PageStatus.GetSMPStatus(Machine.Page1.Beam.Beam1, Machine.Page1.SMPFlags.Moveable_Devices_Allowed_In);
+                                    Console.WriteLine("Beam 1 Moveable Devices Allowed In Flag is {0}.", beam1status);
+                                    break;
+                                case "5":
+                                    beam1status = PageStatus.GetSMPStatus(Machine.Page1.Beam.Beam1, Machine.Page1.SMPFlags.Setup_Beam);
+                                    Console.WriteLine("Beam 1 Setup Beam Flag is {0}.", beam1status);
+                                    break;
+                                case "6":
+                                    beam1status = PageStatus.GetSMPStatus(Machine.Page1.Beam.Beam1, Machine.Page1.SMPFlags.Stable_Beams);
+                                    Console.WriteLine("Beam 1 Stable Beams Flag is {0}.", beam1status);
+                                    break;
+                                default:
+                                    break;
+                            }
+                            break;
+                        case "2":
+                            Console.WriteLine("Which SMP Flag do you want to check?");
+                            var beam2SmpFlags = Enum.GetValues(typeof(Machine.Page1.SMPFlags)).Cast<Machine.Page1.SMPFlags>();
+                            i = 1;
+                            beam2SmpFlags.ToList().ForEach(s => Console.WriteLine(i++ + "." + s.ToString().Replace('_', ' ')));
+                            input = Console.ReadLine();
+                            bool beam2status;
+                            switch (input)
+                            {
+                                case "1":
+                                    beam2status = PageStatus.GetSMPStatus(Machine.Page1.Beam.Beam2, Machine.Page1.SMPFlags.Beam_Presence);
+                                    Console.WriteLine("Beam 2 Beam Presence Flag is {0}.", beam2status);
+                                    break;
+                                case "2":
+                                    beam2status = PageStatus.GetSMPStatus(Machine.Page1.Beam.Beam2, Machine.Page1.SMPFlags.Global_Beam_Permit);
+                                    Console.WriteLine("Beam 2 Global Beam Permit Flag is {0}.", beam2status);
+                                    break;
+                                case "3":
+                                    beam2status = PageStatus.GetSMPStatus(Machine.Page1.Beam.Beam2, Machine.Page1.SMPFlags.Link_Status_of_Beam_Permits);
+                                    Console.WriteLine("Beam 2 Link Status of Beam Permits Flag is {0}.", beam2status);
+                                    break;
+                                case "4":
+                                    beam2status = PageStatus.GetSMPStatus(Machine.Page1.Beam.Beam2, Machine.Page1.SMPFlags.Moveable_Devices_Allowed_In);
+                                    Console.WriteLine("Beam 2 Moveable Devices Allowed In Flag is {0}.", beam2status);
+                                    break;
+                                case "5":
+                                    beam2status = PageStatus.GetSMPStatus(Machine.Page1.Beam.Beam2, Machine.Page1.SMPFlags.Setup_Beam);
+                                    Console.WriteLine("Beam 2 Setup Beam Flag is {0}.", beam2status);
+                                    break;
+                                case "6":
+                                    beam2status = PageStatus.GetSMPStatus(Machine.Page1.Beam.Beam2, Machine.Page1.SMPFlags.Stable_Beams);
+                                    Console.WriteLine("Beam 2 Stable Beams Flag is {0}.", beam2status);
+                                    break;
+                                default:
+                                    break;
+                            }
                             break;
                         default:
                             break;
