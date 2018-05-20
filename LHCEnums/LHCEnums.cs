81b7149dc1e0171ff0aaf08cb60a54c0309f2e76
Clover <blazelion3@gmail.com>
2018-05-20 16:29:01 -0400

    Changed Cryo Enums, added the invidual magnets in preperation for information retreival from cryostatus class. Updated RF Enums.

--- LHCEnums/LHCEnums.cs
+++ LHCEnums/LHCEnums.cs
@@ -8,24 +8,120 @@ namespace LHCEnums
 {
     public class Machine
     {
-        public enum Sector
+        public class Cryo
         {
-            Sector12,
-            Sector23,
-            Sector34,
-            Sector45,
-            Sector56,
-            Sector67,
-            Sector78,
-            Sector81
+            public enum Sectors
+            {
+                Sector12,
+                Sector23,
+                Sector34,
+                Sector45,
+                Sector56,
+                Sector67,
+                Sector78,
+                Sector81
+            }
+
+            public enum PCPermit
+            {
+                S12,
+                S23,
+                S34,
+                S45,
+                S56,
+                S67,
+                S78,
+                S81
+            }
+
+
+            public class Magnets
+            {
+
+                public enum Magnet
+                {
+                    //Sector12
+                    CMITR1,
+                    CSITR1,
+                    CMMSR1,
+                    CSMSR1,
+                    CMAR12,
+                    CSAR12,
+                    CMMSL2,
+                    CSMSL2,
+                    CMITL2,
+                    CSITL2,
+                    //Sector23
+                    CMITR2,
+                    CSITR2,
+                    CMMSR2,
+                    CSMSR2,
+                    CMAML3,
+                    CSAML3,
+                    //Sector34
+                    CMARM3,
+                    CSARM3,
+                    CMMSL4,
+                    CSMSL4,
+                    //Sector45
+                    CMMSR4,
+                    CSMSR4,
+                    CMAR45,
+                    CSAR45,
+                    CMMSL5,
+                    CSMSL5,
+                    CMITL5,
+                    CSITL5,
+                    //Sector56
+                    CMITR5,
+                    CSITR5,
+                    CMMSR5,
+                    CSMSR5,
+                    CMAR56,
+                    CSAR56,
+                    CMMSL6,
+                    CSMSL6,
+                    //Sector67
+                    CMMSR6,
+                    CSMSR6,
+                    CMAML7,
+                    CSAML7,
+                    //Sector78
+                    CMAMR7,
+                    CSAMR7,
+                    CMMSL8,
+                    CSMSL8,
+                    CMITL8,
+                    CSITL8,
+                    //Sector81
+                    CMITR8,
+                    CSITR8,
+                    CMMSR8,
+                    CSMSR8,
+                    CMAR81,
+                    CSAR81,
+                    CMMSL1,
+                    CSMSL1,
+                    CMITL1,
+                    CSITL1
+                }
+
+            }
         }
 
-        public enum RF
+        public class RF
         {
-            Sector1L4,
-            Sector2L4,
-            Sector1R4,
-            Sector2R4
+            public enum Sectors
+            {
+                Sector1L4,
+                Sector2L4,
+                Sector1R4,
+                Sector2R4
+            }
+
+            public enum Cryo
+            {
+            }
         }
 
         public enum Beam
